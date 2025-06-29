608003448d28f8d4e72bfc5e8b358fd6b7f57263
Clement <clement.darribeau@gmail.com>
2025-06-29 22:04:17 +0200

    creation de la feature "heros" et des tests associé

--- test/test_rpg.py
+++ test/test_rpg.py
@@ -1,5 +1,6 @@
 import unittest
 from src.character_type.character import modele_character
+from src.character_type.heros import Heros
 
 class TestRpg(unittest.TestCase):
     def test_true_equals_true(self):
@@ -34,6 +35,61 @@ class TestPersonnage(unittest.TestCase):
         blesse.pv -= 5
         blesse.soin(2)
         self.assertEqual(blesse.pv,7)
+
+class TestHeros(unittest.TestCase):
+    def test_initialisation_heros(self):
+        chevalier = Heros("Perceval", "Humain")
+        self.assertEqual(chevalier.race, "Humain")
+        self.assertEqual(chevalier.pv, 10)
+        self.assertEqual(chevalier.pv_max, 10)
+        self.assertEqual(chevalier.attaque, 1)
+        self.assertEqual(chevalier.defense, 0)
+        self.assertEqual(chevalier.magie, 10)
+        self.assertEqual(chevalier.magie_max, 10)
+        self.assertEqual(chevalier.xp, 0)
+        self.assertEqual(chevalier.niveau, 1)
+
+    def test_lvl_up(self):
+        chevalier = Heros("Ivain le chevalier au lion", "Humain")
+        chevalier.lvl_up()
+        self.assertEqual(chevalier.niveau, 2)
+        self.assertEqual(chevalier.pv_max, 15)
+        self.assertEqual(chevalier.pv, 15)
+        self.assertEqual(chevalier.attaque, 2)
+        self.assertEqual(chevalier.defense, 1)
+        self.assertEqual(chevalier.magie_max, 13)
+        self.assertEqual(chevalier.magie, 13)
+        self.assertEqual(chevalier.xp, 0)
+
+    def test_lancer_sorts_avec_magie(self):
+        mage = Heros("Merlin", "Demi demon")
+        cible = modele_character("Lancelot", "Traitre")
+        cible.defense = 2
+        cible.pv = 10
+        succes = mage.lancer_sorts(cible, 5)
+        self.assertTrue(succes)
+        self.assertEqual(mage.magie, 5)
+        self.assertEqual(cible.pv, 7) 
+
+    def test_lancer_sorts_pas_assez_magie(self):
+        mage = Heros("Merlin", "Demi demon")
+        cible = modele_character("Bandit", "Humain")
+        mage.magie = 3
+
+        succes = mage.lancer_sorts(cible, 5)
+        self.assertFalse(succes)
+        self.assertEqual(mage.magie, 3) 
+        self.assertEqual(cible.pv, 10)  
+
+    def test_lancer_sorts_degats_pas_negatifs(self):
+        morg = Heros("Morgane", "Fee")
+        cible = modele_character("soldat", "humain")
+        cible.defense = 10
+        
+        succes = morg.lancer_sorts(cible, 5) 
+        self.assertTrue(succes)
+        self.assertEqual(cible.pv, 10)
+
 
 
 if __name__ == '__main__':
