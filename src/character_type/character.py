74e7a4bf03e3b5ae73ca803fdcfd75a901814995
Clement <clement.darribeau@gmail.com>
2025-06-29 21:29:16 +0200

    implementation de la classe character

--- src/character_type/character.py
+++ src/character_type/character.py
@@ -1,6 +1,31 @@
 class modele_character:
 
-    def __init__(self):
-        self
+    def __init__(self, name, race):
+        self.name = name
+        self.race = race
+        self.pv_max = 10
+        self.pv = 10
+        self.attaque = 1
+        self.defense = 0
 
- 
+
+
+    #montant est un nombre ou "full" pour un soin total
+    def soin(self, montant):
+      if montant == "full":
+        self.pv = self.pv_max
+      elif isinstance(montant, int):
+        self.pv += montant
+        if self.pv > self.pv_max:
+            self.pv = self.pv_max
+      else:
+          raise ValueError("L'argument est invalide, entrez un nombre ou 'full'")
+
+
+
+    #les pv sont baissé d'une valeur egale a l'attaque moins la defense adverse
+    def frappe(self, defenseur):
+        degats = self.attaque - defenseur.defense
+        if degats < 0:
+            degats = 0
+        defenseur.pv -= degats
